0d6c329ae78b6a9bd793b0ada86e5f17e89381ce
Cauazitodev16 <rubislinscaua@gmail.com>
2026-04-27 14:11:33 -0300

    build: criando tela de resultados de uma biblioteca comunitaria

---
+++ script.js
@@ -0,0 +1,91 @@
+const livrosEncontrados = [
+    {
+        id: 1,
+        titulo: "Dom Casmurro",
+        autor: "Machado de Assis",
+        imagem: 'https://m.media-amazon.com/images/I/61x1ZHomWUL._SY522_.jpg',
+        disponivel: true
+    },
+    {
+        id: 2, 
+        titulo: "O Pequeno Príncipe",
+        autor: "Antoine de Saint-Exupéry",
+        imagem: 'https://m.media-amazon.com/images/I/81TmOZIXvzL._SY522_.jpg',
+        disponivel: false
+    },
+    {
+        id: 3,
+        titulo: "1984",
+        autor: "George Orwell",
+        imagem: 'https://m.media-amazon.com/images/I/61t0bwt1s3L._SY522_.jpg',
+        disponivel: true
+    },
+    {
+        id: 4,
+        titulo: "A revolução dos bichos",
+        autor: "George Orwell",
+        imagem: 'https://m.media-amazon.com/images/I/91BsZhxCRjL._SY522_.jpg',
+        disponivel: true
+    },
+];
+
+const resultadosBusca = document.getElementById("resultadosBusca");
+
+function exibirLivros(livros){
+    livros.forEach(livro => {
+        const card = document.createElement("div");
+        card.classList.add("card-livro");
+
+        const img = document.createElement("img");
+        img.setAttribute("src", livro.imagem);
+
+        const titulo = document.createElement("h3");
+        titulo.textContent = livro.titulo;
+
+        const autor = document.createElement("p");
+        autor.textContent = `Autor: ${livro.autor}`;
+
+        const status = document.createElement("p");
+        status.classList.add("status");
+
+        if(livro.disponivel){
+            status.textContent = "Disponível";
+            status.classList.add("disponivel");
+        }else{
+            status.textContent = "Indisponível";
+            status.classList.add("indisponivel");
+        }
+
+        const botao = document.createElement ("button");
+        botao.textContent = "Reservar";
+        botao.setAttribute("data-id", livro.id);
+
+        if(!livro.disponivel){
+            botao.disabled = true;
+        }
+
+        botao.addEventListener("click", () => {
+            botao.textContent = "Reservado!";
+            botao.disabled = true;
+
+        status.textContent = "Indisponível";
+
+            status.classList.remove("disponivel");
+            status.classList.add("indisponivel");
+            livro.disponivel = false;
+        console.log(`Livro reservado: ${livro.titulo}`);
+
+        });
+
+        card.appendChild(img);
+        card.appendChild(titulo);
+        card.appendChild(autor);
+        card.appendChild(status);
+        card.appendChild(botao);
+
+        resultadosBusca.appendChild(card);
+
+    },);
+}
+
+exibirLivros(livrosEncontrados);
